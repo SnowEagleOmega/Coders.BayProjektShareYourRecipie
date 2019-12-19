3289ccc94aed660381609c856c013a57fb71a32b
Samuel W <55083529+SnowEagleOmega@users.noreply.github.com>
2019-12-19 14:31:01 +0100

    Add files via upload

--- weihnachten/database/weihnachten.sql
+++ weihnachten/database/weihnachten.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 09. Dez 2019 um 14:36
+-- Erstellungszeit: 19. Dez 2019 um 14:29
 -- Server-Version: 10.4.6-MariaDB
 -- PHP-Version: 7.1.32
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `weihnachten`
 --
+CREATE DATABASE IF NOT EXISTS `weihnachten` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `weihnachten`;
 
 -- --------------------------------------------------------
 
@@ -35,6 +37,13 @@ CREATE TABLE `account` (
   `nachname` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Daten für Tabelle `account`
+--
+
+INSERT INTO `account` (`id`, `email`, `vorname`, `nachname`) VALUES
+(57, 'dodo@dodo.com', 'Dodo', 'Dodomann');
+
 -- --------------------------------------------------------
 
 --
@@ -47,8 +56,21 @@ CREATE TABLE `rezept` (
   `rezeptname` varchar(100) DEFAULT NULL,
   `art` varchar(100) DEFAULT NULL,
   `zutaten` varchar(1000) DEFAULT NULL,
-  `zubereitung` varchar(10000) NOT NULL
+  `zubereitung` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `rezept`
+--
+
+INSERT INTO `rezept` (`id`, `accountID`, `rezeptname`, `art`, `zutaten`, `zubereitung`) VALUES
+(1, 57, 'Punschkrapfen', 'Mehlspeise', 'Punsch\r\nKrapfen', 'Punsch mit Krapfen genieÃŸen\r\n'),
+(2, 57, 'Punsch', 'Getraenk', 'Gluck', 'Gluck'),
+(3, 57, 'Tester', 'Getraenk', 'goog', 'Drooop\r\n'),
+(4, 57, 'Punschkrapfen', 'Mehlspeise', 'Glasur\r\nTeig', 'Glasur und Teig mischen'),
+(5, 57, 'DOdo', 'Mehlspeise', 'DOdo', 'Dodo'),
+(6, 57, 'Langos', 'Snack', '700g Mehl\r\n0.5l Milch\r\n10g Hefe\r\n1Prise Salz', 'FÃ¼r den Langosch bereiten wir zuerst ein Dampfel zu. Dazu einen Teil der lauwarmen Milch mit der Hefe verrÃ¼hren, etwas Mehl dazu bis es leicht bindet, und an einem warmen Ort 15 Min. gehen lassen.\r\nDanach abwechselnd das restliche Mehl und die Milch zugeben, salzen und kneten - geht am besten mit der KÃ¼chenmaschine mit einem Knethaken. Sobald ein eher fester Brotteig entsteht, den Teig mit Mehl bestÃ¤uben und wieder eine halbe Stunde an einem warmen Ort gehen lassen.\r\nAus dem Teig beliebig groÃŸe ca. 1,5 cm hohe, runde Teigfladen formen - am besten mit der Hand - der Rand kann dabei etwas hÃ¶her bleiben - den Teig mit ein wenig Ã–l bestreichen und nochmals fÃ¼r rund 20 Minuten gehen lassen.\r\nAnschlieÃŸend in einer beschichteten Pfanne das Ã–l (Fingerhoch) erhitzen und den Teig von beiden Seiten goldbraun (ca. 2 Min.) backen - funktioniert natÃ¼rlich auch sehr gut in einer Fritteuse. Die fertig gebackenen Langosch aus dem Fett nehmen und auf KÃ¼chenpapier abtropfen lassen.'),
+(7, 57, 'DOdo', 'Mehlspeise', 'Dodo', 'Dodo');
 
 --
 -- Indizes der exportierten Tabellen
@@ -76,13 +98,13 @@ ALTER TABLE `rezept`
 -- AUTO_INCREMENT für Tabelle `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT für Tabelle `rezept`
 --
 ALTER TABLE `rezept`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints der exportierten Tabellen
